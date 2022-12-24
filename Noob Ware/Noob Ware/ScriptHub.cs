28ab6a071ccdffd245a0975cc677ace1c0b387dd
Zach <99155118+NooobLorod@users.noreply.github.com>
2022-12-24 12:47:38 -0500

    Add files via upload

---
+++ Noob Ware/Noob Ware/ScriptHub.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeAreDevs_API;
+
+namespace Noob_Ware
+{
+    public partial class ScriptHub : Form
+    {
+        private object lastPoint;
+
+        public ScriptHub()
+        {
+            InitializeComponent();
+        }
+
+        private void ScriptHub_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient wb = new WebClient();
+            string Script = wb.DownloadString("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()");
+            module.SendLuaScript(Script);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Noob Ware/Noob Ware/module.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Noob_Ware
+{
+    internal class module
+    {
+        internal static void SendLuaScript(string script)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
